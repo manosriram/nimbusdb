e70c6b90ae4abd64d8d4961598e74408ce096bf8
Mano Sriram <mano.sriram0@gmail.com>
2023-12-28 01:50:23 +0530

    made progress in fixing merge process

--- examples/b.go
+++ examples/b.go
@@ -1,13 +1,16 @@
 // package main
 
-// import "fmt"
+// import (
+// "fmt"
+// "time"
+// )
+
+// func HasTimestampExpired(timestamp int64) bool {
+// tstamp := time.Unix(0, timestamp).UnixNano()
+// now := time.Now().UnixNano()
+// return tstamp < now
+// }
 
 // func main() {
-// // t := crc32.MakeTable(crc32.IEEE)
-// // data := []byte("test")
-// // hash := crc32.Checksum(data, t)
-// // fmt.Println(utils.Int32ToByte(int32(hash)))
-
-// s := "helloworld"
-// fmt.Println(string(s[6]))
+// fmt.Println(HasTimestampExpired(1704307674058084000))
 // }
